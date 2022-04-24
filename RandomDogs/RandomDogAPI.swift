51ad62cdebbdbe7c07953250bb74a6b3568e6e33
Caroline <carolinealaw@gmail.com>
2022-04-23 21:47:57 -0700

    init commit

---
+++ RandomDogs/RandomDogAPI.swift
@@ -0,0 +1,52 @@
+//
+//  RandomDogAPI.swift
+//  RandomDogs
+//
+//  Created by Spud on 4/23/22.
+//
+
+import Foundation
+
+class RandomDogAPI {
+    
+    let urlSession: URLSession = URLSession(configuration: .default)
+    
+    
+    init() {
+        
+    }
+    
+    func randomDog(callback: @escaping (String) -> ()) {
+        
+        guard let url = URL(string: "https://dog.ceo/api/breeds/image/random") else {
+            print("there was a problem getting the url")
+            return
+        }
+        
+        
+        
+        let datatask = urlSession.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print(error)
+            }
+            
+            guard let data = data else {
+                print("there was a problem getting data")
+                return
+            }
+            
+            let randomDog = try? JSONDecoder().decode(RandomDogDataModel.self, from: data)
+            
+            guard let imageURL = randomDog?.message else {
+                print("There was a problem getting the dog image")
+                return
+            }
+            
+            callback(imageURL)
+        }
+        
+        datatask.resume()
+        
+    }
+    
+}
---
+++ RandomDogs/RandomDogDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  RandomDogDataModel.swift
+//  RandomDogs
+//
+//  Created by Spud on 4/23/22.
+//
+
+import Foundation
+
+struct RandomDogDataModel: Decodable {
+    let message: String
+    let status: String
+}
